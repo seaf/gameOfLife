8e9fa8158bb098684616908846a2157f237849c9
Geoff H <seafra.haugan@gmail.com>
2018-12-23 19:05:29 -0800

    Iterator that returns set of neighbors for a given cell. Detects under/overflow via "checked" keyword, but exception is not catchable to continue iteration.

--- GameOfLife/Engine/generationEngine.cs
+++ GameOfLife/Engine/generationEngine.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 
 namespace GameOfLife.Engine
 {
-    class GenerationEngine : IGenerationEngine
+    public class GenerationEngine : IGenerationEngine
     {
         public HashSet<Tuple<ulong, ulong>> Advance(HashSet<Tuple<ulong, ulong>> liveCells)
         {
-            // TODO
             return liveCells;
+        }
+
+        private IEnumerable<Tuple<ulong, ulong>> GetValidNeighbors(Tuple<ulong, ulong> cell)
+        {
+            var notDone = true;
+            var validNeighbors = new List<Tuple<ulong, ulong>>();
+            var enumerator = this.GetNeighbors(cell).GetEnumerator();
+            while (notDone)
+            {
+                try
+                {
+                    notDone = enumerator.MoveNext();
+                    validNeighbors.Add(enumerator.Current);
+                }
+                catch (OverflowException) { }
+            }
+
+            return validNeighbors;
+        }
+
+        // TODO: Consider as an extension method
+        // TODO: Alternative to avoid throwing in under/overflow cases: check Item1/2 values against 0 and ulong.MaxValue
+        // TODO: Consider a "Cell" class to replace tuple that would hold logic for handling neighbors and checking under/overflow. This
+        private IEnumerable<Tuple<ulong, ulong>> GetNeighbors(Tuple<ulong, ulong> cell)
+        {
+            yield return Tuple.Create(checked(cell.Item1 - 1), checked(cell.Item2 - 1));
+            yield return Tuple.Create(checked(cell.Item1 - 1), checked(cell.Item2));
+            yield return Tuple.Create(checked(cell.Item1 - 1), checked(cell.Item2 + 1));
+            yield return Tuple.Create(checked(cell.Item1), checked(cell.Item2 - 1));
+            yield return Tuple.Create(checked(cell.Item1), checked(cell.Item2));
+            yield return Tuple.Create(checked(cell.Item1), checked(cell.Item2 + 1));
+            yield return Tuple.Create(checked(cell.Item1 + 1), checked(cell.Item2 - 1));
+            yield return Tuple.Create(checked(cell.Item1 + 1), checked(cell.Item2));
+            yield return Tuple.Create(checked(cell.Item1 + 1), checked(cell.Item2 + 1));
         }
     }
 }
