53fe0782666147d7856b3e683bfa347c44dfbfa8
Geoff H <seafra.haugan@gmail.com>
2018-12-31 01:10:13 -0800

    Added additional unit tests for basic still life and oscillators under standard Game of Life rules.

--- GameOfLife.Test/Unit/StandardRulesGenerationStrategyTests.cs
+++ GameOfLife.Test/Unit/StandardRulesGenerationStrategyTests.cs
@@ -39,6 +39,105 @@ namespace GameOfLife.Test.Unit
             nextGen.Should().BeEquivalentTo(beehive);
         }
 
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationLoafStillLifeShouldRemainUnchanged(IGenerationStrategy strategyUnderTest)
+        {
+            var loaf = new HashSet<Cell> {
+                new Cell(0, 1),
+                new Cell(0, 2),
+                new Cell(1, 0),
+                new Cell(1, 3),
+                new Cell(2, 1),
+                new Cell(2, 3),
+                new Cell(3, 2)};
+
+            var nextGen = strategyUnderTest.AdvanceGeneration(loaf);
+            nextGen.Should().BeEquivalentTo(loaf);
+        }
+
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationBoatStillLifeShouldRemainUnchanged(IGenerationStrategy strategyUnderTest)
+        {
+            var boat = new HashSet<Cell> {
+                new Cell(0, 0),
+                new Cell(0, 1),
+                new Cell(1, 0),
+                new Cell(1, 2),
+                new Cell(2, 1)};
+
+            var nextGen = strategyUnderTest.AdvanceGeneration(boat);
+            nextGen.Should().BeEquivalentTo(boat);
+        }
+
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationTubStillLifeShouldRemainUnchanged(IGenerationStrategy strategyUnderTest)
+        {
+            var tub = new HashSet<Cell> {
+                new Cell(0, 1),
+                new Cell(1, 0),
+                new Cell(1, 2),
+                new Cell(2, 1)};
+
+            var nextGen = strategyUnderTest.AdvanceGeneration(tub);
+            nextGen.Should().BeEquivalentTo(tub);
+        }
+
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationBlinkerOscillatorShouldRepeatInTwoGenerations(IGenerationStrategy strategyUnderTest)
+        {
+            var blinker = new HashSet<Cell> {
+                new Cell(0, 1),
+                new Cell(1, 1),
+                new Cell(2, 1)};
+
+            AssertOsscillatorRepeatsInTwoGenerations(blinker, strategyUnderTest);
+        }
+
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationToadOscillatorShouldRepeatInTwoGenerations(IGenerationStrategy strategyUnderTest)
+        {
+            var toad = new HashSet<Cell> {
+                new Cell(1, 1),
+                new Cell(1, 2),
+                new Cell(1, 3),
+                new Cell(2, 0),
+                new Cell(2, 1),
+                new Cell(2, 2)};
+
+            AssertOsscillatorRepeatsInTwoGenerations(toad, strategyUnderTest);
+        }
+
+        [DynamicData(nameof(StrategiesWithStandardRules))]
+        [DataTestMethod, TestCategory(TestCategories.Unit)]
+        public void AdvanceGenerationBeaconOscillatorShouldRepeatInTwoGenerations(IGenerationStrategy strategyUnderTest)
+        {
+            var beacon = new HashSet<Cell> {
+                new Cell(0, 0),
+                new Cell(0, 1),
+                new Cell(1, 0),
+                new Cell(1, 1),
+                new Cell(2, 2),
+                new Cell(2, 3),
+                new Cell(3, 2),
+                new Cell(3, 3)};
+
+            AssertOsscillatorRepeatsInTwoGenerations(beacon, strategyUnderTest);
+        }
+
+        private static void AssertOsscillatorRepeatsInTwoGenerations(HashSet<Cell> initialState, IGenerationStrategy strategyUnderTest)
+        {
+            var gen1 = strategyUnderTest.AdvanceGeneration(initialState);
+            gen1.Should().NotBeEquivalentTo(initialState);
+
+            var gen2 = strategyUnderTest.AdvanceGeneration(gen1);
+            gen2.Should().BeEquivalentTo(initialState);
+        }
+
         private static IEnumerable<object[]> StrategiesWithStandardRules
         {
             get
